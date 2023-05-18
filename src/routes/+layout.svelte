51cd700c0a4182d02956b7e2d0b3874edcd18312
Cameron Courtney <cameroncourtney@utexas.edu>
2023-05-18 18:02:18 -0500

    sticky header and navbar functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,39 +9,47 @@
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+	function sectionScroll(section: string) {
+		const ref = document.getElementById(section);
+		console.log(ref);
+		ref?.scrollIntoView({behavior: 'smooth'});
+	}
 </script>
 
+<!-- Main wrapper component around application -->
 <AppShell>
-	<AppBar id="shell" gap="gap-8">
+	<AppBar gap="gap-8" class="sticky top-0">
 		<svelte:fragment slot="lead">
 			<div />
 		</svelte:fragment>
 		<svelte:fragment>
 			<!-- Header Contents -->
-			<div id="headerContainer">
-				<button type="button" class="btn !bg-transparent">
+			<div id="navContainer">
+				<button type="button" class="btn !bg-transparent" on:click={() => sectionScroll("about")} >
 					<h1 class="h3" id="header">About</h1>
 				</button>
-				<button type="button" class="btn !bg-transparent">
+				<button type="button" class="btn !bg-transparent" on:click={() => sectionScroll("experience")}>
 					<h1 class="h3" id="header">Experience</h1>
 				</button>
-				<button type="button" class="btn !bg-transparent">
+				<button type="button" class="btn !bg-transparent" on:click={() => sectionScroll("projects")} >
 					<h1 class="h3" id="header">Projects</h1>
 				</button>
-				<button type="button" class="btn !bg-transparent">
+				<button type="button" class="btn !bg-transparent" on:click={() => sectionScroll("contact")}>
 					<h1 class="h3" id="header">Contact</h1>
 				</button>
 			</div>
 		</svelte:fragment>
-		<svelte:fragment slot="trail"
-			><LightSwitch class="white" width="w-14" height="h-7" /></svelte:fragment
-		>
+		<svelte:fragment slot="trail">
+			<LightSwitch class="white" width="w-14" height="h-7" />
+		</svelte:fragment>
 	</AppBar>
 	<slot />
 </AppShell>
 
 <style>
-	#headerContainer {
+	/* Formats the buttons/sections of the site as a flex-box. */
+	#navContainer {
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,19 +5,21 @@
 <!-- <div id="dummyBar">
 	<h1>Cameron Courtney's Website</h1>
 </div> -->
-<div class="card p-4">About Me</div>
-<div class="card p-4 bg-gradient-to-br variant-gradient-tertiary-secondary">Experience</div>
-<div class="card p-4 bg-gradient-to-br variant-gradient-tertiary-primary">Contact</div>
-<div class="card p-4 bg-gradient-to-br variant-gradient-secondary-primary">Projects</div>
-<div class="card p-4 bg-gradient-to-br variant-gradient-secondary-tertiary">Projects</div>
-<div class="card p-4 bg-gradient-to-br variant-gradient-primary-secondary">Contact</div>
-<div class="card p-4 bg-gradient-to-br variant-gradient-primary-tertiary">Contact</div>
-<div class="card p-4 variant-filled-primary">blue</div>
-<div class="card p-4 variant-filled-secondary">Contact</div>
-<div class="card p-4 variant-filled-tertiary">Contact</div>
-<div class="card p-4 variant-filled-success">Contact</div>
-<div class="card p-4 variant-filled-warning">Contact</div>
-<div class="card p-4 variant-filled-error">Contact</div>
+<div id="layout">
+	<div id="about" class="card p-4">About Me</div>
+	<div id ="experience" class="card p-4 bg-gradient-to-br variant-gradient-tertiary-secondary">Experience</div>
+	<div class="card p-4 bg-gradient-to-br variant-gradient-tertiary-primary">Contact</div>
+	<div id="projects" class="card p-4 bg-gradient-to-br variant-gradient-secondary-primary">Projects</div>
+	<div class="card p-4 bg-gradient-to-br variant-gradient-secondary-tertiary">Projects</div>
+	<div class="card p-4 bg-gradient-to-br variant-gradient-primary-secondary">Contact</div>
+	<div class="card p-4 bg-gradient-to-br variant-gradient-primary-tertiary">Contact</div>
+	<div class="card p-4 variant-filled-primary">blue</div>
+	<div class="card p-4 variant-filled-secondary">Contact</div>
+	<div class="card p-4 variant-filled-tertiary">Contact</div>
+	<div class="card p-4 variant-filled-success">Contact3</div>
+	<div class="card p-4 variant-filled-warning">Contact4</div>
+	<div id="contact" class="card p-4 variant-filled-error">Contact3</div>
+</div>
 
 <style>
 	/* #dummyBar {
@@ -27,12 +29,12 @@
 		gap: 1rem;
 		justify-content: center;
 		align-items: center;
-	}
+	} */
 
 	#layout {
 		display: flex;
 		flex-direction: column;
 		margin: 1rem;
 		gap: 1rem;
-	} */
+	}
 </style>
